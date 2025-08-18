e46d57603ecb1118985a347654e9c8f114322f8b
ohhyunsok <terecal@daum.net>
2025-08-18 10:14:10 +0900

    update for 헤더 메뉴2

--- src/widgets/header/ui/AppHeader.tsx
+++ src/widgets/header/ui/AppHeader.tsx
@@ -1,8 +1,8 @@
 import { cookies } from "next/headers";
 import { AppHeaderClient } from "./AppHeaderClient";
 
-export default function AppHeader() {
-    const c = cookies();
+export default async function AppHeader() {
+    const c = await cookies();
     const isAuthed = Boolean(c.get("auth")?.value);
     const username = c.get("user")?.value ?? "guest";
     return <AppHeaderClient isAuthed={isAuthed} username={username} />;
